d70a2717d70f4c546ccf8ac08e53f7f8f934bf71
geicibarham <geicianecosta43@gmail.com>
2022-02-13 14:22:14 -0500

    quotes display on its container properly now

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,6 @@
 var newQuotebutton = document.querySelector("#new-quote-button");
 var firstQuotecontainer = document.querySelector("#first-quote-container");
+var firstQuotep = document.querySelector("#first-quote");
 
 
 
@@ -9,22 +10,27 @@ var apiUrl = "https://goquotes-api.herokuapp.com/api/v1/random?count=1";
 fetch(apiUrl)
 .then(function (response) {
 response.json().then(function(data){
-// console.log(data.quotes[0]);
-
+    // return (data);
+    // console.log(data.quotes[0]);
+ 
     // firstQuotecontainer.innerHTML = data.quotes[0];
-
- displayQuotes(data)
-
+    displayQuotes(data)
 })
 
 })
 var displayQuotes = function (data) {
-console.log(data);
 
-firstQuotecontainer.innerHTML = data.quotes[0];
+//    console.log(data.quotes[0].text)
+// console.log(data);
+// console.log(data.quotes[0]);
+// // console.log(data.quotes.text);
+// var quote = data.quotes[0];
+// const myJSON = JSON.stringify(data.quotes[0]);
+
+firstQuotep.innerHTML = data.quotes[0].text;
 
 }
 
 
-
+// newQuotebutton.addEventListener("click" , getQuotes);
 newQuotebutton.addEventListener("click" , displayQuotes);
