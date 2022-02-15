df51168ccec8c830d76523eab7040248686cef9d
david curtis <heythereimdavidcurtis@gmail.com>
2022-02-15 17:58:57 -0500

    rewote fucntions to await. Added a handleAPI function go gather all the data on one event handler

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,189 +5,53 @@ var authorFirstquote = document.querySelector("#author-1");
 var secondQuote = document.querySelector("#second-quote");
 var authorTwo = document.querySelector("#author-2");
 var btnTwo = document.querySelector("#btn-2");
+
 var persons = ["yoda", "chef"];
 var person = persons[Math.floor(Math.random() * persons.length)];
 
-// first fetch request
-var getQuotes = function () {
-  var apiUrl = "https://goquotes-api.herokuapp.com/api/v1/random?count=1";
+var quoteData = {};
 
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        firstQuotep.innerText = data.quotes[0].text;
-        authorFirstquote.innerText = data.quotes[0].author;
-        JSON.stringify(data.quotes[0].text);
-        var firstAuth = data.quotes[0].author;
-        var firstQuote = data.quotes[0].text;
-        console.log(firstQuote);
+async function getQuotes() {
+  const apiUrlOne = "https://goquotes-api.herokuapp.com/api/v1/random?count=1";
+  return (await fetch(apiUrlOne)).json();
+}
 
-        // arrays to be mapped
-        const quoteArray = firstQuote.split(" ");
-        const spacerQuoteArray = [
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-          "%20",
-        ];
+newQuotebutton.addEventListener("click", async () => {
+  try {
+    const data = await handleAPICalls();
+    firstQuotep.innerText = data.firstQuote.quotes[0].text;
+    authorFirstquote.innerText = data.firstQuote.quotes[0].author;
+  } catch (error) {
+    console.log("Error!");
+    console.log(error);
+  }
+});
 
-        // combines the arrays
-        var arrayCombined = $.map(quoteArray, function (stringVal, index) {
-          return [stringVal, spacerQuoteArray[index]];
-        });
-
-        // string to be used in get request
-        let callString = arrayCombined.join("");
-        console.log(callString);
-        fetchFinalQuote(callString, firstAuth);
-        return callString + firstAuth;
-      });
-    }
-  });
+const handleAPICalls = async () => {
+  let quotesFirstResponse = await getQuotes();
+  console.log(quotesFirstResponse.quotes[0].text);
+  let firstQuote = quotesFirstResponse.quotes[0].text;
+  let callString = firstQuote.replaceAll(" ", "%20");
+  async function getSecondQuote() {
+    let apiUrl =
+      "https://api.funtranslations.com/translate/" + person + ".json?text=";
+    return (await fetch(apiUrl + callString)).json();
+  }
+  let quotesSecondResponse = await getSecondQuote();
+  quoteData = {
+    firstQuote: quotesFirstResponse,
+    secondQuote: quotesSecondResponse,
+  };
+  console.log("Quotes fetched");
+  console.log(quoteData);
+  return quoteData;
 };
 
-newQuotebutton.addEventListener("click", getQuotes);
-
-// url to use in second fecth request
-let apiUrl =
-  "https://api.funtranslations.com/translate/" + person + ".json?text=";
-
-//  second fetch request
-let fetchFinalQuote = function (callString, firstAuth) {
-  console.log(callString);
-  fetch(apiUrl + callString).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        // print quote and author to page
-        console.log(data);
-        secondQuote.innerText = data.contents.translated;
-        authorTwo.innerText = firstAuth;
-      });
-    }
-  });
-};
+btnTwo.addEventListener("click", async () => {
+  const data = quoteData;
+  secondQuote.innerText = data.secondQuote.contents.translated;
+  authorTwo.innerText = data.firstQuote.quotes[0].author;
+});
 
 //
 //This section is for the slide show
