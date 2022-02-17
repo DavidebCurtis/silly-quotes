aa385f9ddce20306fa5a5bcb71a0f5382f702f88
geicibarham <geicianecosta43@gmail.com>
2022-02-17 18:10:59 -0500

    local storage display

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,9 +5,13 @@ var authorFirstquote = document.querySelector("#author-1");
 var secondQuote = document.querySelector("#second-quote");
 var authorTwo = document.querySelector("#author-2");
 var translateQuoteButton = document.querySelector("#btn-2");
-var savedQuotes = document.querySelector("#saved-quotes");
+
+const slider = document.querySelector(".slider");
+
+const alertPlaceholder = document.querySelector("#alert-placeholder");
+
 // picks the person the quote will be translated to
-var persons = ["Yoda,Chef"];
+var persons = ["yoda", "chef"];
 var person = persons[Math.floor(Math.random() * persons.length)];
 
 // quote data global variable
@@ -54,50 +58,99 @@ const handleAPICalls = async () => {
   console.log("Quotes fetched");
   console.log(quoteData);
   return quoteData;
-  // saveToLocal(quoteData )
 };
 
 // on translate button click insert translated quote and author
 translateQuoteButton.addEventListener("click", async () => {
   const data = quoteData;
   secondQuote.innerText = data.secondQuote.contents.translated;
-  authorTwo.innerText = data.firstQuote.quotes[0].author;
+  authorTwo.innerText = data.secondQuote.contents.translation;
+  console.log(authorTwo);
+  saveQuotes();
+  displaySavedQuotes();
 });
 
-//This section is for the slide show
-const slider = document.querySelector(".slider");
+var savedQuotes = JSON.parse(localStorage.getItem("saved quotes")) || [];
 
-const leftArrow = document.querySelector(".left");
-const rightArrow = document.querySelector(".right");
-const indicatorParents = document.querySelector(".controls ul");
-var sectionIndex = 0;
-
-document.querySelectorAll(".controls li").forEach(function (indicator, ind) {
-  indicator.addEventListener("click", function () {
-    sectionIndex = ind;
-    document.querySelector(".controls .selected").classList.remove("selected");
-    indicator.classList.add("selected");
-    slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
-  });
-});
-
-leftArrow.addEventListener("click", function () {
-  sectionIndex = sectionIndex > 0 ? sectionIndex - 1 : 0;
-  document.querySelector(".controls .selected").classList.remove("selected");
-  indicatorParents.children[sectionIndex].classList.add("selected");
-  slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
-});
-
-rightArrow.addEventListener("click", function () {
-  sectionIndex = sectionIndex < 3 ? sectionIndex + 1 : 3;
-  document.querySelector(".controls .selected").classList.remove("selected");
-  indicatorParents.children[sectionIndex].classList.add("selected");
-  slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
-});
-
-
-//save to local storage
-function saveToLocal (quoteData) {
-console.log(quoteData);
+// save quote to localstorage
+function saveQuotes() {
+  const data = quoteData;
+  const saved = {
+    quote: data.secondQuote.contents.translated,
+    author: data.secondQuote.contents.translation,
+    
+  };
+  savedQuotes.push(saved);
+  localStorage.setItem("saved quotes", JSON.stringify(savedQuotes));
 }
+
+// load quote to element
+function displaySavedQuotes() {
+  var savecontainer = document.querySelector("#saved-quotes");
+  savecontainer.innerHTML = ""
+
+  if (savedQuotes.length === 0) {
+    var alertMessage = document.createElement("p");
+    alertMessage.textContent = "No Quotes yet."
+    alertMessage.setAttribute("class" ,"alert");
+    savecontainer.append(alertMessage);
+  } else {
+
+
+  for (let index = 0; index < savedQuotes.length; index++) {
+ 
+
+    // create p for quote
+    const quoteContainer = document.createElement("section");
+    const makeP = document.createElement("p");
+    makeP.innerText = savedQuotes[index].quote;
+    makeP.setAttribute("class","quote-Saved ");
+
+    const makeAuthor = document.createElement("h4");
+    makeAuthor.innerText= savedQuotes[index].author;
+    makeAuthor.style.textAlign = "right";
+    makeAuthor.style.textTransform = "capitalize";
+    makeAuthor.setAttribute("class","saved-author ");
+   
+  quoteContainer.setAttribute("class" , "saved");
+
+  // savecontainer.setAttribute("class" , "saved");
+    // create p for author
+    quoteContainer.appendChild(makeP);
+    quoteContainer.appendChild(makeAuthor);
+  savecontainer.appendChild( quoteContainer);
+
+  }
+}
+}
+
+displaySavedQuotes();
+// //This section is for the slide show
+// const leftArrow = document.querySelector(".left");
+// const rightArrow = document.querySelector(".right");
+// const indicatorParents = document.querySelector(".controls ul");
+// var sectionIndex = 0;
+
+// document.querySelectorAll(".controls li").forEach(function (indicator, ind) {
+//   indicator.addEventListener("click", function () {
+//     sectionIndex = ind;
+//     document.querySelector(".controls .selected").classList.remove("selected");
+//     indicator.classList.add("selected");
+//     slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
+//   });
+// });
+
+// leftArrow.addEventListener("click", function () {
+//   sectionIndex = sectionIndex > 0 ? sectionIndex - 1 : 0;
+//   document.querySelector(".controls .selected").classList.remove("selected");
+//   indicatorParents.children[sectionIndex].classList.add("selected");
+//   slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
+// });
+
+// rightArrow.addEventListener("click", function () {
+//   sectionIndex = sectionIndex < 3 ? sectionIndex + 1 : 3;
+//   document.querySelector(".controls .selected").classList.remove("selected");
+//   indicatorParents.children[sectionIndex].classList.add("selected");
+//   slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
+// });
 //End of slide show
